6f7fb45fd6ae5909cc11048369af02a1daf7b025
Eiichi Fujimoto <work.eiichi.co.jp@gmail.com>
2018-09-05 02:03:54 +0900

    既存コードをアップ

---
+++ README.md
@@ -0,0 +1,9 @@
+# Google Apps Script 用の Redmine ライブラリ
+
+スクリプト ID
+
+```
+1K4mG6xQ21YUN1_BUX16eCClNvZDqWESzQjALaPjthycNA1iTGHSH5Okj
+```
+
+[RedmineTools (gas.noraneko.work)](https://script.google.com/d/1K4mG6xQ21YUN1_BUX16eCClNvZDqWESzQjALaPjthycNA1iTGHSH5Okj/edit?usp=drive_web)
---
+++ src/Issues.js
@@ -0,0 +1,110 @@
+// 設定値
+var API_URL = '' // https://example.com
+var API_KEY = ''
+
+/**
+ * issueを発行
+ * @param {*} issueJsonData 
+ */
+function createIssue (issueJsonData) {
+  const METHOD = 'POST'
+  const PATH = '/issues.json'
+  var options = {
+    'payload': JSON.stringify(issueJsonData)
+  }
+
+  var result = send_(METHOD, PATH, options)
+  Logger.log(JSON.parse(result.getContentText()))
+
+  return result
+}
+
+// function fileUpload (issueJsonData) {
+//  const METHOD = 'POST'
+//  const PATH = '/uploads.json'
+//  var options = {
+//     'headers': { 
+//      'Content-type': 'application/json',
+//    }
+//    'payload': JSON.stringify(issueJsonData)
+//  }
+//
+//  var result = send_(METHOD, PATH, options)
+//  Logger.log(JSON.parse(result.getContentText()))
+//  
+//  return result
+// }
+
+/**
+ * issueを取得
+ * @param {*} issueId 
+ */
+function getIssue (issueId, include) {
+  const METHOD = 'GET'
+  const PATH = '/issues/' + issueId + '.json?include=' + include
+
+  var result = send_(METHOD, PATH, {})
+  Logger.log(JSON.parse(result.getContentText()))
+
+  return result
+}
+
+/**
+ * APIを叩く 共通処理
+ * @param {*} method 
+ * @param {*} path 
+ * @param {*} methodOptions 
+ */
+function send_ (method, path, methodOptions) {
+  var options = {
+    'method': method,
+    'headers': {
+      'Content-type': 'application/json',
+      'X-Redmine-API-Key': API_KEY
+    }
+  }
+
+  // 追加オプションがあれば入れる
+  if ('payload' in methodOptions) {
+    options.payload = methodOptions.payload
+  }
+  if ('headers' in methodOptions) {
+    if ('Content-type' in methodOptions.headers) {
+      options.headers['Content-type'] = methodOptions.headers['Content-type']
+    }
+  }
+
+  // APIKeyの確認
+  if (API_URL == '') {
+    throw new Error('API_URL is not set')
+  }
+
+  // APIKeyの確認
+  if (API_KEY == '') {
+    throw new Error('API_KEY is not set')
+  }
+
+  Logger.log('endpoint: ' + API_URL + path)
+  Logger.log('Option: ')
+  Logger.log(options)
+
+  return UrlFetchApp.fetch(API_URL + path, options)
+}
+
+// ----------
+
+/**
+ * API URLをセット
+ * @param {*} apiUrl 
+ */
+function setApiUrl (apiUrl) {
+  API_URL = apiUrl
+}
+
+/**
+ * API Keyをセット
+ * @param {*} apiKey 
+ */
+function setApiKey (apiKey) {
+  API_KEY = apiKey
+}
